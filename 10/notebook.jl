d6a6e57579c1521234124db42e5e09e2f646eef1
Leo Stenzel <leostenzel@users.noreply.github.com>
2023-12-10 16:02:35 +0100

    Rewriting parts of 10

--- 10/notebook.jl
+++ 10/notebook.jl
@@ -4,14 +4,46 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 90713aae-4661-4836-8501-0918ee22b789
-using JuliaSyntax
-
 # ╔═╡ fe2cd977-5c8b-41d4-8e98-e02201a52627
 input = ("input" |> readlines .|> x->Vector{Char}(x)) |> x->hcat(x...);
 
 # ╔═╡ 100ccd65-f873-41d4-8a9c-fc25c6cc7eca
 inout_dict = Dict(:north => :south, :south=>:north, :east => :west, :west => :east)
+
+# ╔═╡ 27946889-9d50-4e63-af08-bb9ba4a7a1e1
+begin
+	struct CharPipe{char, indir}
+		loc::CartesianIndex{2}
+	end
+	CharPipe(loc::CartesianIndex{2}, indir) = CharPipe{input[loc], indir}(loc)
+end
+
+# ╔═╡ 5df688e7-f402-42fd-a551-5b2c4ee3dbde
+char(::CharPipe{c, indir}) where {c, indir} = c
+
+# ╔═╡ 6b0a1cb2-01eb-4f67-893a-607843bb2d33
+in_dir(::CharPipe{char, indir}) where {char, indir} = indir
+
+# ╔═╡ 1b83c9f6-8959-4b1b-8c5e-83e6fe42050c
+begin
+	# generic fallback
+	out_dir(::CharPipe) = nothing
+	
+	out_dir(::CharPipe{'7', :south}) = :west
+	out_dir(::CharPipe{'7', :west}) = :south
+
+	out_dir(::CharPipe{'F', :south}) = :east
+	out_dir(::CharPipe{'F', :east}) = :south
+
+	out_dir(::CharPipe{'L', :east}) = :north
+	out_dir(::CharPipe{'L', :north}) = :east
+
+	out_dir(::CharPipe{'J', :north}) = :west
+	out_dir(::CharPipe{'J', :west}) = :north
+
+	out_dir(::CharPipe{'|', indir}) where {indir} = inout_dict[indir]
+	out_dir(::CharPipe{'-', indir}) where {indir} = inout_dict[indir]
+end
 
 # ╔═╡ f5d8ded9-ff10-4a86-b65e-221bd8a77e00
 location(c::CharPipe) = c.loc
@@ -26,49 +58,14 @@ begin
 	move(::Val{:east}, c::CI) where {CI<:CartesianIndex} = CI(c[1]+1, c[2])
 end
 
-# ╔═╡ 1b83c9f6-8959-4b1b-8c5e-83e6fe42050c
-begin
-	# generic fallback
-	out_dir(c::CharPipe) = nothing
-	
-	out_dir(c::CharPipe{'7', :south}) = :west
-	out_dir(c::CharPipe{'7', :west}) = :south
+# ╔═╡ 1a236e6d-f4fb-4576-b93e-0b30907e14bc
+next_loc(c::CharPipe) = move(out_dir(c), location(c))
 
-	out_dir(c::CharPipe{'F', :south}) = :east
-	out_dir(c::CharPipe{'F', :east}) = :south
-
-	out_dir(c::CharPipe{'L', :east}) = :north
-	out_dir(c::CharPipe{'L', :north}) = :east
-
-	out_dir(c::CharPipe{'J', :north}) = :west
-	out_dir(c::CharPipe{'J', :west}) = :north
-
-	out_dir(c::CharPipe{'|', indir}) where {indir} = inout_dict[indir]
-	out_dir(c::CharPipe{'-', indir}) where {indir} = inout_dict[indir]
-
-	in_dir(c::CharPipe{char, indir}) where {char, indir} = indir
-
-	next_loc(c::CharPipe) = move(out_dir(c), location(c))
-end
-
-# ╔═╡ 27946889-9d50-4e63-af08-bb9ba4a7a1e1
-begin
-	struct CharPipe{char, indir}
-		loc::CartesianIndex{2}
-	end
-
-	CharPipe(loc::CartesianIndex{2}, indir) = CharPipe{input[loc], indir}(loc)
-	
-	function Base.iterate(c::CharPipe{char, indir}) where {char, indir}
-		char == 'S' && return nothing
-		
-		n_c = input[next_loc(c)]		
-		n_indir = inout_dict[out_dir(c)]
-		
-		CharPipe{n_c, n_indir}(next_loc(c))
-	end
-
-	char(::CharPipe{c, indir}) where {c, indir} = c
+# ╔═╡ a92bde01-053a-4773-b75e-4ca11e420c31
+function Base.iterate(c::CharPipe{char, indir}) where {char, indir}
+	char == 'S' && return nothing	
+	nc = next_loc(c)
+	CharPipe{input[nc], inout_dict[out_dir(c)]}(nc)
 end
 
 # ╔═╡ 4776733a-cb1a-41d4-a403-5e60289bfe20
@@ -77,18 +74,15 @@ directions = inout_dict |> keys
 # ╔═╡ 89cf7822-0956-4049-a6e8-35e8c6195564
 begin
 	loop = CharPipe[]
-
 	s_loc = findfirst(==('S'), input)
-	
 	next = nothing
 	
 	for dir ∈ directions
 		next = CharPipe(move(dir, s_loc), inout_dict[dir])
-		
 		!isnothing(out_dir(next)) && break
 	end
 
-	while next !== nothing
+	while !isnothing(next)
 		push!(loop, next)
 		next = iterate(next)
 	end
@@ -109,58 +103,52 @@ begin
 	left(dir) = nothing
 	
 	# implementations
+	# We always consider _incoming_ directions
 	left(::Val{:east}) = :south
 	left(::Val{:west}) = :north
 	left(::Val{:north}) = :east
 	left(::Val{:south}) = :west
 
-	# # wrapper
+	# wrapper
 	left(dir::Symbol) = left(Val(dir))
 
 	function left(c::CharPipe{char, indir}) where {char, indir}
 		outdir = out_dir(c)
-		isnothing(outdir) && return setdiff([left(indir)], [indir])
-		setdiff([left(indir), left(inout_dict[outdir])], [indir, outdir])
+		isnothing(outdir) && return [left(indir)]
+		setdiff(left.([indir, inout_dict[outdir]]), [indir, outdir])
 	end
 end
 
 # ╔═╡ e16b2bad-146c-4f1b-b4ce-d190ce0f6b83
 begin
-	left_set = Set{CartesianIndex}()
-	for c ∈ loop[begin:end-1]
-		for dir ∈ left(c)
-			push!(left_set, move(dir, c))
-		end
+	left_locs = CartesianIndex{2}[]
+	for c ∈ loop
+		push!.((left_locs,), move.(left(c), (c,)))
 	end
 
 	# Adding the output direction of the 'S' node explicitly
-	push!(left_set, move(left(in_dir(loop[begin])), loop[end]))
+	push!(left_locs, move(left(in_dir(loop[begin])), loop[end]))
 	
-	left_set = setdiff(left_set, location.(loop))
+	left_locs = setdiff(left_locs, location.(loop))
 end
 
 # ╔═╡ eac8281e-01cd-405e-bf68-11262ea8d08a
 function grow_set(seed, boundary)
 	candidates = copy(seed)
-
 	result = Set{eltype(candidates)}()
 	
 	while !isempty(candidates)
 		c = pop!(candidates)
-
-		c ∈ boundary && continue
-		c ∈ result && continue
-
+		(c ∈ boundary || c ∈ result) && continue
+		
 		push!(result, c)
-		for dir in directions
-			push!(candidates, move(dir, c))
-		end
+		push!.((candidates,), move.(directions, (c,)))
 	end
 	result
 end
 
 # ╔═╡ 5597adcf-f22f-47a5-a121-af0b6cfa30d9
-grow_set(left_set, Set(location.(loop))) |> length
+grow_set(left_locs, Set(location.(loop))) |> length
 
 # ╔═╡ 3ef08350-be20-4f9f-8627-33a71a530fd2
 md"Did we count the inside or the outside…?"
@@ -197,37 +185,17 @@ But that's okay, the last one is hidden in the `S` node.
 # ╔═╡ e1ecdc09-21eb-42a5-a61e-eeecc5a08a5b
 curvature.(loop) |> sum
 
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-JuliaSyntax = "70703baa-626e-46a2-a12c-08ffd08c73b4"
-
-[compat]
-JuliaSyntax = "~0.4.8"
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
-julia_version = "1.9.4"
-manifest_format = "2.0"
-project_hash = "b3e27a3e97c78e04ee6408efa7c2c47d5367c8c2"
-
-[[deps.JuliaSyntax]]
-git-tree-sha1 = "e00e2b013f3bd98d3789f889b9305c1546ecd1ab"
-uuid = "70703baa-626e-46a2-a12c-08ffd08c73b4"
-version = "0.4.8"
-"""
-
 # ╔═╡ Cell order:
-# ╠═90713aae-4661-4836-8501-0918ee22b789
 # ╠═fe2cd977-5c8b-41d4-8e98-e02201a52627
 # ╠═100ccd65-f873-41d4-8a9c-fc25c6cc7eca
 # ╠═27946889-9d50-4e63-af08-bb9ba4a7a1e1
+# ╠═5df688e7-f402-42fd-a551-5b2c4ee3dbde
+# ╠═6b0a1cb2-01eb-4f67-893a-607843bb2d33
 # ╠═1b83c9f6-8959-4b1b-8c5e-83e6fe42050c
+# ╠═1a236e6d-f4fb-4576-b93e-0b30907e14bc
 # ╠═e126451e-ae64-4eb9-9f89-86057dd14b5d
 # ╠═f5d8ded9-ff10-4a86-b65e-221bd8a77e00
+# ╠═a92bde01-053a-4773-b75e-4ca11e420c31
 # ╠═4776733a-cb1a-41d4-a403-5e60289bfe20
 # ╠═89cf7822-0956-4049-a6e8-35e8c6195564
 # ╟─2161308f-c50d-49ea-9f6e-02ffbd7d30fa
@@ -240,5 +208,3 @@ version = "0.4.8"
 # ╠═242bd32b-253f-49bb-85f9-5721d0fe06ee
 # ╟─e7910a83-5964-44b2-9d2e-06a0f15afdd8
 # ╠═e1ecdc09-21eb-42a5-a61e-eeecc5a08a5b
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
